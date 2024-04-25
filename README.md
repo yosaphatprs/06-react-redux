78713056ec20a218da3ef1de3f34e4f47a0ec684
yoreimon <susilojosafat@gmail.com>
2024-04-26 01:39:55 +0700

    W06: Praktikum 2

--- README.md
+++ README.md
@@ -13,3 +13,21 @@ Setelah melakukan install Redux dan Bootstrap, kemudian menulis kode yang ada da
 
 Kemudian jika button diklik, akan muncul modal seperti berikut:
 ![Praktikum 1](/assets-report/01b.png)
+
+### Praktikum 2
+
+Soal
+
+1. Coba akses http://localhost:3000/login, dan klik tombol login. Kemudian lakukan refresh page berkali-kali (jika perlu restart npm run dev nya). Simpulkan apa yang terjadi ?
+
+- Maka state akan tersimpan bahwa telah login atau isLogin = true.
+
+2. Baris 25 dan 30 terdapat method parse(), apa yang terjadi jika kita tidak menggunakan method tersebut?
+
+- Jika tidak menggunakan method tersebut maka tidak ada error.
+
+Hasil:
+![Praktikum 2](/assets-report/02a.png)
+
+Hasil:
+![Praktikum 2](/assets-report/02b.png)
---
+++ pages/login.tsx
@@ -0,0 +1,58 @@
+import { useDispatch, useSelector, Provider } from "react-redux";
+import { setLogin } from '../redux/auth/authSlice';
+import { store } from "../redux/store/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import parse from "html-react-parser";
+
+function LoginCheck() {
+    const { isLogin } = useSelector((state: any) => state.auth);
+    const dispatch = useDispatch();
+
+    function handleAuth(type = 'logout') {
+        if (type === 'logout' || type === 'login') {
+            dispatch(setLogin({ isLogin: type === 'login' ? true : false }));
+        }
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card mt-3">
+                            <div className="card-header">Status Login</div>
+                            <div className="card-body">
+                                {
+                                    isLogin ?
+                                        [ // Jika berhasil login (isLogin = true)
+                                            <>
+                                                <div className="alert alert-success">Yay, berhasil login!!!</div>
+                                                <button className="btn btn-md btn-danger" onClick={() => handleAuth('logout')}>Log Out</button>
+                                            </>
+                                        ]
+                                        :
+                                        [ // Jika telah logout (isLogin = false)
+                                            <>
+                                                <div className="alert alert-danger">Anda telah logout</div>
+                                                <button className="btn btn-md btn-primary" onClick={() => handleAuth('login')}>Log In</button>
+                                            </>
+                                        ]
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function App() {
+    return (
+        <Provider store={store}>
+            <LoginCheck />
+        </Provider>
+    );
+}
+
+export default App;
